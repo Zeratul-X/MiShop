193040efe6260139b11b46098b9efa7737704ab3
Zeratul_X <382018999@qq.com>
2020-07-08 17:28:26 +0800

    gulp打包2020年7月8日17:28:25

---
+++ dist/images/goods/Mi10Pro/blue/小米商城_files/index.js
@@ -0,0 +1,49 @@
+  /**
+   * swiper
+   */
+var mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal', // 垂直切换选项
+      loop: true, // 循环模式选项
+      autoplay:true,
+      // 如果需要分页器
+      pagination: {
+          el: '.swiper-pagination',
+      },
+
+      // 如果需要前进后退按钮
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+
+})
+
+/**
+ * 顶部滚动到500px显示返回顶部
+ */
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100){
+        $('#search').css({
+            'position': 'sticky',
+            'top': '0',
+            'background':'#666 !important',
+        });
+    }
+    if ($(window).scrollTop() > 500)
+    {
+        $('.backtotop').css({
+            display: 'block'
+        });
+    } else {
+        $('.backtotop').css({
+            display: 'none'
+        });
+    }
+});
+
+/**
+ * 回到顶部按钮事件
+ */
+$('.backtotop').click(() => {
+    $(window).animate({ 'scrollTop': '0' }, 'slow');
+})
